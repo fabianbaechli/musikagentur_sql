c29fce0987d0dce485e2b5c8b48209c828e722b8
Fabian Bächli <habian.baechli@gmail.com>
2016-12-19 10:35:40 +0100

    + Temporary Finished DDL

--- DDL.sql
+++ DDL.sql
@@ -7,8 +7,17 @@ CREATE TABLE tbl_ort (
   plz varchar(10),
   name_ortschaft(50),
   kanton varchar(50),
-  land varchar(5)
-)
+  land varchar(5),
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tbl_instrument (
+  id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  name_instrument varchar(50),
+
+  PRIMARY KEY (id)
+);
 
 CREATE TABLE tbl_person (
 	id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
@@ -20,7 +29,27 @@ CREATE TABLE tbl_person (
   strasse varchar(50),
 
 	PRIMARY KEY (id),
-  FOREIGN KEY (fk_ort) REFERENCES tbl_ort (id)
+  FOREIGN KEY(fk_ort) REFERENCES tbl_ort (id)
+);
+
+CREATE TABLE tbl_band (
+  id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  fk_promoter int(11),
+  name_band varchar(50),
+
+  PRIMARY KEY (id),
+  FOREIGN KEY(fk_promoter) REFERENCES tbl_person(id)
+);
+
+CREATE TABLE tbl_veranstaltung (
+  id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  name_veranstaltung varchar(50),
+  von Date,
+  bis Date,
+  fk_veranstalter,
+
+  PRIMARY KEY (id)
+  FOREIGN KEY(fk_veranstalter) REFERENCES tbl_person (id)
 );
 
 CREATE TABLE tbl_musiker_band (
@@ -29,7 +58,31 @@ CREATE TABLE tbl_musiker_band (
   fk_band int(11),
   fk_instrument int(11),
 
-  FOREIGN KEY (fk_musiker) REFERENCES tbl_person (id),
-  FOREIGN KEY (fk_band) REFERENCES tbl_band (id),
-  FOREIGN KEY (fk_instrument) REFERENCES tbl_instrument (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY(fk_musiker) REFERENCES tbl_person (id),
+  FOREIGN KEY(fk_band) REFERENCES tbl_band (id),
+  FOREIGN KEY(fk_instrument) REFERENCES tbl_instrument (id)
+);
+
+CREATE TABLE tbl_dj (
+  id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  kuenstler_name varchar(50),
+  plattform (50),
+  fk_person,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY(fk_person) REFERENCES tbl_person (id),
+);
+
+CREATE TABLE tbl_auftritt (
+  id int(11) NOT NULL AUTO_INCREMENT UNIQUE,
+  fk_band int(11),
+  fk_veranstaltung int(11),
+  fk_dj int(11)
+  von datetime,
+  bis datetime,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY(fk_band) REFERENCES tbl_band (id),
+  FOREIGN KEY(fk_dj) REFERENCES tbl_person (id)
 );
