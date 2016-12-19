7c4d46b5430ca8927e65cefcc88dad16dab720eb
Fabian Bächli <habian.baechli@gmail.com>
2016-12-19 11:35:00 +0100

    + Progress In DML & DDL

---
+++ DML.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_ort (plz, name_ortschaft, provinz, land) VALUES
+  ("8320", "Fehraltorf", "Zürich", "Schweiz"),
+  ("8442", "Hettlingen", "Zürich", "Schweiz"),
+  ("8303", "Bassersdorf", "Zürich", "Schweiz"),
+  ("8053", "Zürich Stadt", "Zürich", "Schweiz"),
+  ("90079", "Los Angeles", "California", "United States Of America"),
+  ("10011", "New York", "Colorado", "United States Of America");
