0e19ca1bcb1dc5cb843b8fb23cc150a924430822
FabriceBosshard <f.bosshard@bosshard.com>
2017-01-09 10:24:45 +0100

    +added view

--- Views.sql
+++ Views.sql
@@ -4,3 +4,9 @@ join tbl_veranstaltung V on V.id = A.fk_veranstaltung
 join tbl_Band B on B.id = A.fk_band
 join tbl_Person DJ on DJ.id = A.fk_dj
 join tbl_Person P on P.id = B.fk_promoter;
+
+Create View BandMitglieder as select
+M.vorname,M.nachname, B.name_band,P.vorname, P.nachname, I.name_instrument from tbl_Personen M
+join tbl_musiker_band MB on MB.fk_musiker = P.id
+join tbl_instrumente I on I.id = MB.fk_instrument
+join tbl_band B on B.id = MB.fk_band
